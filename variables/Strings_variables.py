3b4701edec7e1df5ad5f38dc29c974e8eeaf95e4
ST4RL1GHT <dryuakita@gmail.com>
2026-01-04 19:59:09 +0900

    strings_variables

---
+++ variables/Strings_variables.py
@@ -0,0 +1,9 @@
+#string variables
+name = "nathan"
+greeting = "hello"
+age = "fourteen"
+height = "five foot two"
+country = " japan"
+favorite_food = "steak, avocado, banana, eggs, beef, lamb, meats, salmom, sushi, sashimi, and more"
+liked_colors = "red, blue, green, black, purple, green, silver, gold"
+hobies = "gaming, codding, reading, writting, eating, basketball"
