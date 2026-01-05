a013ebd212a6292c8575d1edc9a7afdeb2b6cbab
ST4RL1GHT <dryuakita@gmail.com>
2026-01-05 11:52:09 +0900

    Add string manipulation examples in Python

---
+++ Strings/Python_strings.py
@@ -0,0 +1,8 @@
+text = 'Python is easy to learn, I wanna keep learning and making progress for the future.'
+
+print(text.upper())
+print(text.lower())
+print(text.find('learn'))
+print(text.replace('wanna', 'would like to'))
+print(text.replace('the', 'my'))
+print(text[0:23])
