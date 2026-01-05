10ca14189921a07e8f007bc6ae327fc1ecf044f3
ST4RL1GHT <dryuakita@gmail.com>
2026-01-05 11:42:02 +0900

    Examples_of_Split_mothod

---
+++ Methods/Split_method.py
@@ -0,0 +1,15 @@
+words1 = 'this is an apple'
+words2 = 'this is a pen'
+words3 = 'boom'
+words4 = 'apple pen'
+words5 = 'this is a pineapple'
+words6 = 'this is a pen'
+words7 = 'boom'
+words8 = 'pineapple pen'
+words9 = 'whis is pen apple'
+words10 = 'this is a pineapple pen'
+words11 = 'bbom'
+words12 = 'pen apple pineapple pen'
+
+stuff = [words1, words2, words3, words4, words5, words6, words7, words8, words9, words10, words11, words12]
+print(stuff)
