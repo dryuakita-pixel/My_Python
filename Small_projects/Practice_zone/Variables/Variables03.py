54e68ea31ab349acc5d5b2491bd1336441d3e372
ST4RL1GHT <dryuakita@gmail.com>
2026-01-21 21:07:43 +0900

    Add lists of birds and land animals

---
+++ Small_projects/Practice_zone/Variables/Variables03.py
@@ -0,0 +1,18 @@
+Birds = [
+    ("Turkey"),
+    ("Chicken"),
+    ("pigoens"),
+    ("Eagles"),
+    ("Parrot"),
+]
+
+Land_animals = [
+    ("Cows"),
+    ("Tiger"),
+    ("HOrse"),
+    ("Cat"),
+    ("Dogs"),
+    ("pig"),
+]
+
+print(Birds + Land_animals)
