6beb7561f4d29b3ba7ff52667ae10cb51ba66487
ST4RL1GHT <dryuakita@gmail.com>
2026-02-22 11:13:08 +0900

    practticing the meaning of "-"  print in list
    Add a list of birds with print statements for each.

---
+++ Methods/List/list3.py
@@ -0,0 +1,22 @@
+bird = [
+    "parrot",
+    "pigeon",
+    "chicken",
+    "duck",
+    "eagle",
+    "owl"
+]
+
+print(bird[0]) #parrot
+print(bird[1]) #pigeon
+print(bird[2]) #chiken
+print(bird[3]) #dick
+print(bird[4]) #eagle
+print(bird[5]) #owl
+
+print(bird[-1]) #owl
+print(bird[-2]) #eagle
+print(bird[-3]) #duck
+print(bird[-4]) #chiken
+print(bird[-5]) #pigeon
+print(bird[-6]) #parrot
