a95daace53fe6424f012387013a690422ebec940
ST4RL1GHT <dryuakita@gmail.com>
2026-02-22 21:41:03 +0900

    Implement while loop for food input
    Fix input prompt typo and improve farewell message.

---
+++ Methods/While_loops/while_loops$.py
@@ -0,0 +1,7 @@
+food = input("Enter a food you like (l to leave): ")
+
+while not food == 'l':
+    print(f"I like that {food} too!")
+    food = input("Enter a onather food you like (l to leave")
+
+print("good bye")
