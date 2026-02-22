7c51d91edfa14aed50d3b59eeca1ad6d5658d12e
ST4RL1GHT <dryuakita@gmail.com>
2026-02-22 11:08:00 +0900

    printing the list

---
+++ Methods/List/list2.py
@@ -0,0 +1,15 @@
+bird = [
+    "parrot",
+    "pigeon",
+    "chicken",
+    "duck",
+    "eagle",
+    "owl"
+]
+
+print(bird[0]) #parrot
+print(bird[1]) #pigeon
+print(bird[2]) #chiken
+print(bird[3]) #dick
+print(bird[4]) #eagle
+print(bird[5]) #owl
