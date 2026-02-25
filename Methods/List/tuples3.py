951980eafb5a4674b29d8096b9896050d4a62319
ST4RL1GHT <dryuakita@gmail.com>
2026-02-25 22:22:33 +0900

    Create tuples3.py

---
+++ Methods/List/tuples3.py
@@ -0,0 +1,14 @@
+#counting the value on the list
+meats = (
+    "beef",
+    "pork",
+    "chiken",
+    "fish"
+)
+
+# print(dir(meats)) #You can use the dir function.
+# print(help(meats)) #You can use the help fuction.
+# print(len(meats)) #You can use the len function also.
+# print("turkey" in meats)
+
+print(meats.index("beef"))
