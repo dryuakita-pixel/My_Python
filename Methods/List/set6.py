022128f852f4b9fea4b5f443e1a91eac9c970180
ST4RL1GHT <dryuakita@gmail.com>
2026-02-25 21:59:01 +0900

    Add example for clearing all items in a set
    Add example code to demonstrate clearing a set.

---
+++ Methods/List/set6.py
@@ -0,0 +1,13 @@
+#this is how to remove all the string on set.
+meats = {
+    "beef",
+    "pork",
+    "chiken",
+    "fish"
+}
+
+# meats.add("turkey")
+# meats.remove("pork")
+# meats.pop()
+meats.clear()
+print(meats)
