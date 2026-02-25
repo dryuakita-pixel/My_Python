b810000b33b4f3ce9f9ec2fbe74f643ab04c9b05
ST4RL1GHT <dryuakita@gmail.com>
2026-02-25 21:46:51 +0900

    Create set1.py with a set of meat types
    Added a set of meat types to set1.py with a print statement.

---
+++ Methods/List/set1.py
@@ -0,0 +1,9 @@
+#The different of the list and set is that you cannot duplicate values and it has curly braces unlike the normal one. and it is unordered and immutable
+meats = {
+    "beef",
+    "pork",
+    "chiken",
+    "fish"
+}
+
+print(meats)
