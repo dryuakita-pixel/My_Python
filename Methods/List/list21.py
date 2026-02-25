b73137b71618f88f4dc4b8db5f130caf4c5fe3d1
ST4RL1GHT <dryuakita@gmail.com>
2026-02-25 21:19:53 +0900

    Create list21.py to count string values in a list
    Add a script to count occurrences of meats in a list.

---
+++ Methods/List/list21.py
@@ -0,0 +1,13 @@
+#counting the string value of the list.
+meats = [
+    "beef",
+    "beef",
+    "pork",
+    "chiken",
+    "fish"
+]
+
+print(meats.count("beef"))
+print(meats.count("pork"))
+print(meats.count("turkey"))
+print(meats)
