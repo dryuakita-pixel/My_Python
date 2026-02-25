f490d29e59ce1b9583455f53d238123a240c0f3e
ST4RL1GHT <dryuakita@gmail.com>
2026-02-25 21:51:15 +0900

    Create set of meat types with usage examples
    Added a set of meat types and demonstrated usage of dir, help, and len functions.

---
+++ Methods/List/set2.py
@@ -0,0 +1,11 @@
+#These are the functijon that can be usable.
+meats = {
+    "beef",
+    "pork",
+    "chiken",
+    "fish"
+}
+
+print(dir(meats)) #You can use the dir function.
+print(help(meats)) #You can use the help fuction.
+print(len(meats)) #You can use the len function also.
