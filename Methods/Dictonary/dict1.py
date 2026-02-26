a00f5626dd6c486f60c7e123220d4a5534bae87f
ST4RL1GHT <dryuakita@gmail.com>
2026-02-26 20:24:12 +0900

    Create dict1.py

---
+++ Methods/Dictonary/dict1.py
@@ -0,0 +1,10 @@
+# this is what you can do on the codes.
+cities = {
+    "Japan": "Tokyo",
+    "China": "Hong kong",
+    "Rusia": "Moscow",
+    "America": "Los angelos"
+}
+
+print(dir(cities)) ## you can use this method.
+print(help(cities)) # you can use this method.
