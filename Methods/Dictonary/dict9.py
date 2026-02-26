2cea4b8ed9e2ec01583982532b5cd5c05a5db928
ST4RL1GHT <dryuakita@gmail.com>
2026-02-26 20:58:19 +0900

    Create dict9.py

---
+++ Methods/Dictonary/dict9.py
@@ -0,0 +1,18 @@
+# using the ".value" method.
+cities = {
+    "Japan": "Tokyo",
+    "China": "Hong kong",
+    "Rusia": "Moscow",
+    "America": "Los angelos"
+}
+
+#print(dir(cities)) # you can use this method.
+#print(help(cities)) # you can use this method.
+
+#cities.update({"Chine": "Beijing"})
+#cities.pop("Rusia")
+#cities.popitem()
+#cities.clear()
+
+value = cities.values()
+print(value)
