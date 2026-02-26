9cea7f4937b54937caacc3d0bdd7a9a38b4007b1
ST4RL1GHT <dryuakita@gmail.com>
2026-02-26 21:03:57 +0900

    Create dict13.py

---
+++ Methods/Dictonary/dict13.py
@@ -0,0 +1,22 @@
+# using the clean ver of ".item" on dictionary.
+cities = {
+    "Japan": "Tokyo",
+    "China": "Hong kong",
+    "Rusia": "Moscow",
+    "America": "Los angelos"
+}
+
+#print(dir(cities)) # you can use this method.
+#print(help(cities)) # you can use this method.
+
+#cities.update({"Chine": "Beijing"})
+#cities.pop("Rusia")
+#cities.popitem()
+#cities.clear()
+
+#value = cities.values()
+#for value in cities.values():
+
+#$items = cities.items()
+for key, value in cities.items():
+    print(f"{key}: {value}")
