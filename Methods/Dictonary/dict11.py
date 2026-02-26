959134f9bd56c5e0f5e4380eb73817979d028b09
ST4RL1GHT <dryuakita@gmail.com>
2026-02-26 21:01:06 +0900

    using the ".items" method.

---
+++ Methods/Dictonary/dict11.py
@@ -0,0 +1,21 @@
+# using the ".items" method.
+cities = {
+    "Japan": "Tokyo",
+    "China": "Hong kong",
+    "Rusia": "Moscow",
+    "America": "Los angelos"
+}
+
+#print(dir(cities)) # you can use this method.
+#print(help(cities)) # you can use this method.
+
+#cities.update({"Chine": "Beijing"})
+#cities.pop("Rusia")
+#cities.popitem()
+#cities.clear()
+
+#value = cities.values()
+#for value in cities.values():
+
+items = cities.items()
+print(items)
