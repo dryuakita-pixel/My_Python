9f45dc07310d481581d42d89b42394fd157efa09
ST4RL1GHT <dryuakita@gmail.com>
2026-03-02 21:13:18 +0900

    #these are the examples of the len in python

---
+++ Methods/len/len2.py
@@ -0,0 +1,21 @@
+#these are the examples of the len in python
+numbers = [
+    "10",
+    "20",
+    "30",
+    "40",
+    "50",
+]
+
+words = {
+    "chiken",
+    "cow",
+    "pig",
+    "parrot",
+    "owl"
+}
+
+print(len(numbers))
+print(len(words))
+
+print(f"the 'number' length is {len(numbers)}, And the length of the word are {len(words)}")
