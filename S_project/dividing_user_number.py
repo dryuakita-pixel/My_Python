89ac90b2ffd28d89e2e340f823bf8a1aae4af718
ST4RL1GHT <dryuakita@gmail.com>
2026-04-15 14:15:02 +0900

    Add dividing_user_number function for user input

---
+++ S_project/dividing_user_number.py
@@ -0,0 +1,11 @@
+def dividing_user_number():
+    print("Wellcome to the subtracting user number program!")
+    
+    user = int(input("Please enter a number: "))
+    dividing = int(input("Please enter a number to divide it by: "))
+
+    result = user / dividing
+
+    print(f"The result: {user} / {dividing} = {result}")
+
+dividing_user_number()
