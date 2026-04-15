dd09549a8c53f28d34dafe49c82bdab5bcb25f60
ST4RL1GHT <dryuakita@gmail.com>
2026-04-15 14:00:11 +0900

    Add multiplying user number program

---
+++ S_project/multiplying_user_number.py
@@ -0,0 +1,11 @@
+def multiplying_user_number():
+    print("Wellcome to the multiplying user number program!")
+    
+    user = int(input("Please enter a number: "))
+    multiplying = int(input("Please enter a number to multiply it by: "))
+
+    result = user * multiplying
+
+    print(f"The result: {user} multiplied by {multiplying} = {result}")
+
+multiplying_user_number()
