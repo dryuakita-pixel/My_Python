7af34e4c201da929f1769da46a7922a44b949258
ST4RL1GHT <dryuakita@gmail.com>
2026-04-15 14:10:53 +0900

    Add subtracting user number program

---
+++ S_project/subtracting_user_number.py
@@ -0,0 +1,11 @@
+def subtracting_user_number():
+    print("Wellcome to the subtracting user number program!")
+    
+    user = int(input("Please enter a number: "))
+    subtracting = int(input("Please enter a number to subtract it by: "))
+
+    result = user - subtracting
+
+    print(f"The result: {user} - {subtracting} = {result}")
+
+subtracting_user_number()
