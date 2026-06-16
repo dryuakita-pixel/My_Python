8a19546c2f972b2b198dd162c9160a838674fe97
ST4RL1GHT <dryuakita@gmail.com>
2026-06-16 21:59:33 +0800

    Add self-introduction function in def1.py

---
+++ practiced_projects/Contacts/def/def1.py
@@ -0,0 +1,8 @@
+def self_introduction():
+    name = input("Enter name: ")
+    age = input("Enter age: ")
+
+    print(f"Hello my name is {name}, and I'm {age} years old.")
+self_introduction()
+print("-" * 50)
+self_introduction()
