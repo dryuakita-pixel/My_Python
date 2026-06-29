797d6947fdbfe049724b083d5bd57482bb908dd1
ST4RL1GHT <dryuakita@gmail.com>
2026-06-30 00:25:59 +0800

    This is what I learned today. : June 30, 2026

---
+++ Daily_projects.uplo.py/Def/def.2.py
@@ -0,0 +1,21 @@
+#1
+def say_hi():
+    print("Hi")
+
+say_hi()
+say_hi()
+
+#2
+def favorite_game(game_name):
+    print(f"My favorite game is {game_name}")
+
+favorite_game("Sniper_arena")
+
+#3
+def draw_line(line):
+    line = "--------------------"
+    print(line)
+
+draw_line("line")
+print("Welcome")
+draw_line("line")
