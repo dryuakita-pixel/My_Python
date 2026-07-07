3d5c5c7fd5a128d5c210d67c93ca91ef4c1f5ad7
ST4RL1GHT <dryuakita@gmail.com>
2026-07-07 23:32:51 +0800

    Create Liked_Games function to display game names
    Added a function to print liked games with a parameter.

---
+++ Daily_projects.uplo.py/Def/def.3.py
@@ -0,0 +1,9 @@
+def Liked_Games(Game_name):
+    print(f"I played {Game_name} earlier.")
+
+Liked_Games("Sniper_Arena")
+Liked_Games("Bedwars")
+Liked_Games("Minecrafr")
+
+#The "(Game_name)" on the top of the 'def' is called "parameter"
+#It can be used in like 'print("f")' and call the function at the botton with "(parameter)" on it.
