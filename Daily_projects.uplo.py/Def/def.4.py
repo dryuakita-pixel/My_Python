8ce3591bc71c5a7793849dc78bb57d21cef57f51
ST4RL1GHT <dryuakita@gmail.com>
2026-07-08 00:28:14 +0800

    Create def.4.py

---
+++ Daily_projects.uplo.py/Def/def.4.py
@@ -0,0 +1,25 @@
+#mission 1
+def greet(names):
+    print(names)
+
+greet("cyrex")
+greet("Babe")
+greet("Nathan")
+
+#mission 2
+def add(a, b):
+    print(a + b)
+    
+add(5, 8)
+add(100, 250)
+add(500, 500)
+
+#mission 3
+def can_vote(age):
+    if age > 18:
+        print("You can vote")
+    else:
+        print("You cannot vote yet")
+
+can_vote(14)
+can_vote(20)
