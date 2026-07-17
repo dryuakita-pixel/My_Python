1f5b447a2b9d53796504948c86035ff79b88356b
ST4RL1GHT <dryuakita@gmail.com>
2026-07-17 19:30:12 +0800

    Add basic arithmetic functions for addition, subtraction, multiplication, and division

---
+++ Daily_projects.uplo.py/Def/def.5.py
@@ -0,0 +1,27 @@
+#addtion ver
+def addtion(a, b):
+    return a + b
+
+answer = addtion(6, 7)
+print(answer)
+
+#subtraction ver
+def subtraction(a, b):
+    return a - b
+
+answer = subtraction(6, 7)
+print(answer)
+
+#multiplication ver
+def multiply(a, b):
+    return a * b
+
+answer = multiply(6, 7)
+print(answer)
+
+#division ver
+def divide(a, b):
+    return a / b
+
+answer = divide(6, 7)
+print(answer)
