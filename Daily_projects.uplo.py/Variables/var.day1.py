ba7ae28bcd31bb05aed541fbbcb0fec3f3f3b7ca
ST4RL1GHT <dryuakita@gmail.com>
2026-07-27 00:17:16 +0800

    Add initial variables and print statements in var.day1.py

---
+++ Daily_projects.uplo.py/Variables/var.day1.py
@@ -0,0 +1,28 @@
+print("Hello, Nathan!")
+
+name = "Nathan"
+age = 14
+country = "Japan"
+fav_game = "rblx"
+fav_lang = "python"
+
+#printing the variables
+print(name)
+print(age)
+print(country)
+print(fav_game)
+print(fav_lang)
+
+name = "Nathan"
+age = 14
+fav_food = "cow"
+dream_job = "freedom"
+fav_hobby = "training"
+
+print(f"Hello my name is {name}, and I am {age}!\nMy dream job is to having {dream_job}\nI love {fav_food} especially steaks\nI also like to go {fav_hobby}")
+
+print(f"Hello I am {name}!", end=":")
+print(f"And I am {age}.", sep=f"I love {fav_hobby}.")
+
+print(f"{name}\n" * 3)
+print(f"{name}" + f"{age}")
