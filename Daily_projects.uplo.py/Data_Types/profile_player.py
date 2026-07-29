e0d4c6942b22647ed2d2fa889310abe36e83a4a4
ST4RL1GHT <dryuakita@gmail.com>
2026-07-29 23:35:08 +0800

    Add profile_player.py with player attributes and types

---
+++ Daily_projects.uplo.py/Data_Types/profile_player.py
@@ -0,0 +1,14 @@
+User_name = "blablu"
+levol = 1
+health = 100
+is_alive = False
+
+print(User_name)
+print(levol)
+print(health)
+print(is_alive)
+
+print(type(User_name))
+print(type(levol))
+print(type(health))
+print(type(is_alive))
