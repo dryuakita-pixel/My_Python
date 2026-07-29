505dae6c3569d9cecb0cfccca7bc1a05849a103e
ST4RL1GHT <dryuakita@gmail.com>
2026-07-29 23:27:24 +0800

    Add Level 1 day Python exercises for data types

---
+++ Daily_projects.uplo.py/Data_Types/Level_1_day.py
@@ -0,0 +1,40 @@
+#1.
+name = ("Nathan", "Jaz", "Lopez")
+print(name[2])
+
+#2.
+hundreds = {100,200,300,400,500,600}
+hundreds.add(700)
+hundreds.add(800)
+hundreds.add(900)
+print(hundreds)
+hundreds.remove(300)
+
+print(hundreds)
+
+#3.
+name = "Nathan"
+age = 14
+height = 162.8
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+#4
+fav_food = "beef"
+fav_color = "green"
+fav_animal = "cow"
+fav_subject = "English"
+
+print(fav_food)
+print(fav_color)
+print(fav_animal)
+print(fav_subject)
