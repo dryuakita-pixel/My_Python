e2229f94067c94af4589e0f865256c766baf82af
ST4RL1GHT <dryuakita@gmail.com>
2026-08-08 16:00:03 +0800

    Add age, score, battery, and height checks

---
+++ Daily_projects.uplo.py/Daily/day7.py
@@ -0,0 +1,31 @@
+#1
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("you are an adult.")
+else: 
+    print("you are a minor")
+
+#2
+score = int(input("Enter your score: "))
+
+if score >= 75: 
+    print("You passed")
+else:
+    print("You failed")
+
+#3
+battery = int(input("Enter Battery Percentage: "))
+
+if battery <= 20:
+    print("Low battery!")
+else:
+    print("high battery!")
+  
+#4
+height = int(input("Enter your height in cm: "))
+
+if height >= 180:
+    print("Great height for basketball!")
+else:
+    print("Can be better!")
