4793dae0c9505673cc99a94c416180b2e5c72f68
ST4RL1GHT <dryuakita@gmail.com>
2026-08-10 19:45:33 +0800

    Add day10.review.py with various examples

---
+++ Daily_projects.uplo.py/Daily/day10.review.py
@@ -0,0 +1,45 @@
+#1
+number = 9
+
+print(number % 2)
+
+#2
+number = 9
+
+print(number % 2)
+
+#3
+score = 70
+
+if score >= 75:
+    print("Passed")
+else:
+    print("Failed")
+
+#4
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+
+if age >= 18:
+    print("adult")
+else:
+    print("minor")
+
+#5
+number = int(input("Enter numbers: "))
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+#6
+name = input("Enter student name: ").strip()
+math = int(input("Enter Math score: "))
+english = int(input("Enter English score: "))
+
+print(math // english)
+print(name)
+print(math)
+print(english)
