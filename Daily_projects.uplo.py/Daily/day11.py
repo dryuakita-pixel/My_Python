ab3c785b1421e51f7173422b8191874d127cc0ed
ST4RL1GHT <dryuakita@gmail.com>
2026-08-11 17:57:03 +0800

    Create day11.py

---
+++ Daily_projects.uplo.py/Daily/day11.py
@@ -0,0 +1,47 @@
+#1
+score = int(input("Enter score: "))
+
+if score >= 90:
+    print("Exellent!")
+elif score >= 80:
+    print("Great!")
+elif score >= 75:
+    print("Passed!")
+else:
+    print("Failed.")
+
+#2
+temp = int(input("Enter temperature: "))
+
+if temp >= 30:
+    print("Hot")
+elif temp >= 20:
+    print("warm")
+elif temp >= 10:
+    print("cool")
+else:
+    print("cold")
+
+#3
+level = int(input("Enter your level: "))
+
+if level >= 50:
+    print("Expert")
+elif level >= 30:
+    print("Advance")
+elif level >= 10: 
+    print("Intermediate")
+else:
+    print("Beginner")
+
+#4
+score = int(input("Enter your score: "))
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 75:
+    print("C")
+else:
+    print("f")
