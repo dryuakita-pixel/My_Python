27c2b491e2588197c7a70e807ff3107cc5771f8f
ST4RL1GHT <dryuakita@gmail.com>
2026-08-26 13:11:16 +0800

    Add day.18.py with various number printing examples

---
+++ Daily_projects.uplo.py/Daily/day.18.py
@@ -0,0 +1,21 @@
+#1
+for number in range(5):
+    print(number)
+
+#2
+for number in range(5):
+    print(number, "Hello")
+
+#3
+for number in range(1, 6):
+    print(number)
+
+#4
+for number in range(1, 6):
+  print(number * 2)
+
+#5
+for number in range(5, 0, -1):
+    print(number)
+
+print("Go")
