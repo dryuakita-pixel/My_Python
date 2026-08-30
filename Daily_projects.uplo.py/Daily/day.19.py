f857e72bf4bb0e5b581248a278e7907509687aa7
ST4RL1GHT <dryuakita@gmail.com>
2026-08-30 20:32:33 +0800

    Add day.19.py with multiple range examples

---
+++ Daily_projects.uplo.py/Daily/day.19.py
@@ -0,0 +1,19 @@
+#1
+for number in range(3, 8):
+    print(number)
+
+#2
+for number in range(1, 11, 2):
+    print(number)
+
+#3
+for number in range(5, 35, 5):
+    print(number)
+
+#4
+for number in range(10, 0, -2):
+    print(number)
+
+#5
+for number in range(2, 22, 2):
+    print(number)
