8b877d5083cc83db6794ae895aef756c266cad87
ST4RL1GHT <dryuakita@gmail.com>
2026-08-31 19:57:26 +0800

    Create day.20.review.py

---
+++ Daily_projects.uplo.py/Daily/day.20.review.py
@@ -0,0 +1,29 @@
+#1
+number = 1
+
+while number <= 3:
+    print(number)
+    number = number + 1
+
+#2
+for number in range(2, 6):
+    print(number)
+
+#3
+for number in range(10, 0, -2):
+    print(number)
+
+#4
+number = 1
+
+while number <= 5:
+    print(number)
+    number = number + 1
+
+#5
+for number in range(5):
+    print("python")
+
+#6
+for number in range(10, 60, 10):
+    print(number)
