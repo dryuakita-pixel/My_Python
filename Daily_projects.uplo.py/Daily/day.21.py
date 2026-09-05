7165cb364002ee7d025612e74bf0fe218b21bfe6
ST4RL1GHT <dryuakita@gmail.com>
2026-09-05 12:21:25 +0800

    Add multiple loops with break and continue examples

---
+++ Daily_projects.uplo.py/Daily/day.21.py
@@ -0,0 +1,48 @@
+#1
+for number in range(1, 10):
+    if number == 5:
+        break
+
+    print(number)
+
+#2
+for number in range(1, 10):
+    if number == 7:
+        break
+
+    print(number)
+
+#3
+for number in range(1, 6):
+    if number == 3:
+        continue
+
+    print(number)
+
+#4
+for number in range(1, 10):
+    if number == 7:
+        break
+
+    print(number)
+
+#5
+for number in range(1, 10):
+    if number == 5:
+        continue
+
+    print(number)
+
+#6
+for number in range(1, 11):
+    if number == 5:
+        continue
+
+    print(number)
+
+#7
+for number in range(1, 11):
+    if number == 5:
+        break
+
+    print(number)
