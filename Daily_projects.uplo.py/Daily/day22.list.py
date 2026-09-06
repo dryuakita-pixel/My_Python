2e3e4e47eed56247e02a810b2cfa8094fb3dbb4e
ST4RL1GHT <dryuakita@gmail.com>
2026-09-06 11:28:54 +0800

    Create day22.list.py with list operations examples
    Added a new Python script that demonstrates list operations including creation, indexing, modification, and length calculation for games and favorite foods.

---
+++ Daily_projects.uplo.py/Daily/day22.list.py
@@ -0,0 +1,34 @@
+#1
+games = ["Minecraft", "Roblox", "Fortnite"]
+
+print(games)
+
+#2
+games = ["Minecraft", "Roblox", "Fortnite"]
+
+print(games[1])
+
+#3
+games = ["Minecraft", "Roblox", "Fortnite"]
+
+games[2] = "Bedwars"
+
+print(games)
+
+#4
+games = ["Minecraft", "Roblox", "Fortnite"]
+
+games[2] = "Bedwars"
+
+print(len(games))
+
+#5
+fav_foods = ["steak", "avocado", "eggs", "salmon", "banana"]
+
+print(fav_foods[0])  # Output: steak
+print(fav_foods[4])  # Output: banana
+
+fav_foods[2] = "honey"  # Change "eggs" to "honey"
+print(fav_foods)  # Output: ['steak', 'avocado', 'honey', 'salmon', 'banana']
+
+print(len(fav_foods)) # Output: 5
