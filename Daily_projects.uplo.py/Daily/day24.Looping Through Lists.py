95a3f47250518578d905022f6c4633eca1d1cba5
ST4RL1GHT <dryuakita@gmail.com>
2026-09-12 11:44:47 +0800

    Implement loops to filter and print lists
    Added loops to print numbers and scores based on conditions.

---
+++ Daily_projects.uplo.py/Daily/day24.Looping Through Lists.py
@@ -0,0 +1,27 @@
+#1
+foods = ["steak", "eggs", "banana", "salmon",]
+
+for food in foods:
+    print(food)
+
+#2
+numbers = [5, 10, 15, 20, 25]
+
+for number in numbers:
+    print(number)
+
+#3 find numbers greater than 10
+numbers = [5, 10, 15, 20, 25]
+
+for number in numbers:
+    if number >= 10:
+        print(number)
+
+#4 find numb ers greater than 75
+scores = [65, 82, 91, 74, 95, 60]
+
+for score in scores:
+    if score >= 75:
+        print(score)
+
+  
