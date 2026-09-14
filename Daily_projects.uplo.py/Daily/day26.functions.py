1cd0efb9e29d003060918ee728acaa47aca56a84
ST4RL1GHT <dryuakita@gmail.com>
2026-09-14 20:26:57 +0800

    Create day26.functions.py

---
+++ Daily_projects.uplo.py/Daily/day26.functions.py
@@ -0,0 +1,32 @@
+#1
+def greet():
+    print("Wellcome to Python!")
+
+greet() #output: Wellcome to Python!
+
+#2
+def show_info():
+    print("Name: Nathan")
+    print("Age: 14")
+    print("fav_hobby: basketball")
+
+show_info()
+show_info()
+show_info()
+
+#3
+games = ["Minecraft", "Roblox", "Bedwars"]
+
+def show_games():
+    print(games)
+
+show_games()
+show_games()
+
+#4
+def show_menu():
+    print("===== MENU =====\n1. Start Game\n2. Settings\n3. Exit\n================")
+
+show_menu()
+show_menu()
+show_menu()
