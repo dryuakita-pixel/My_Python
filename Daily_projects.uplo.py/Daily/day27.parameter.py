f177baecb0c61a22ffc6f623ee20ed82fea1b314
ST4RL1GHT <dryuakita@gmail.com>
2026-09-15 12:12:18 +0800

    Add day27.parameter.py with various functions

---
+++ Daily_projects.uplo.py/Daily/day27.parameter.py
@@ -0,0 +1,27 @@
+#1
+def greet(name):
+    print(name)
+
+greet("Nathan") #output: Nathan
+
+#2
+def intruduce(name, age):
+    print(f"My name is {name}, and I am {age} years old.")
+
+intruduce("Nathan", 14) #output: My name is Nathan, and I am 14 years old.
+
+#3
+ames = ["Minecraft", "Roblox", "Bedwars"]
+
+def show_game(game):
+    print(f"My fav game is {game}")
+
+show_game("Minecraft")
+
+#4
+def student_info(name, grade):
+    print(f"===== STUDENT =====\nName: {name}\nGrade: {grade}\n===================")
+
+student_info("Jhon", 67)
+student_info("bane", 90)
+student_info("jacob", 84)
