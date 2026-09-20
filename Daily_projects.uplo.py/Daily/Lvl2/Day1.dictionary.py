022be7dbc9f8a13e2cb00d1ac07233c180c7824d
ST4RL1GHT <dryuakita@gmail.com>
2026-09-20 12:24:13 +0800

    Add dictionary examples for game and student data
    Added a dictionary example with game and student data, including modifications to the game genre and student favorite subject.

---
+++ Daily_projects.uplo.py/Daily/Lvl2/Day1.dictionary.py
@@ -0,0 +1,37 @@
+#1
+game = {
+    "name": "Minecraft",
+    "genre": "Sandbox",
+    "Year": "2011",
+}
+
+print(game["name"])
+print(game["genre"])
+
+#ouput: Minecraft Sandbox
+
+#2
+game = {
+    "name": "Minecraft",
+    "genre": "Sandbox",
+    "Year": "2011",
+}
+
+
+game["genre"] = "Adventure"
+print(game["genre"])
+
+#output: Adventure
+
+#3
+student = {
+    "name": "Zyjhon",
+    "age": "15",
+    "fav_sub": "math"
+}
+
+print(student)
+print(student["name"])
+print(student["age"])
+
+student["fav_sub"] = "Science"
