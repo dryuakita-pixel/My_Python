0859033413e3faff6c91b164160b370348cceedc
ST4RL1GHT <dryuakita@gmail.com>
2026-09-23 18:10:05 +0800

    Create day4.dict.input.key.py

---
+++ Daily_projects.uplo.py/Daily/Lvl2/day4.dict.input.key.py
@@ -0,0 +1,37 @@
+#1
+person = {
+    "name": "Nathan",
+    "age": 14
+}
+
+person["hobby"] = "basketball"
+print(person)
+
+#2
+person = {
+    "name": "Nathan",
+    "age": 14
+}
+
+person["hobby"] = "basketball"
+print(person)
+
+person["age"] = 15
+print(person)
+
+#3
+player = {
+    "name": "Nathan"
+}
+
+player["game"] = input("Enter a game: ")
+player["lvl"] = input("Enter your lvl: ")
+print(player)
+
+#4
+student = {}
+
+student["Name"] = input("Enter your name: ")
+student["age"] = input("Enter your age: ")
+student["fav_subect"] = input("Enter your favorite subject: ")
+print(student)
